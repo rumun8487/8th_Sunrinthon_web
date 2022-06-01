a04a63ba72dbd43ba224762e0b3d8d27aa6cf06b
kyr-min <mkr200477@gmail.com>
2022-06-01 22:22:20 +0900

    new

--- src/Page1.svelte
+++ src/Page1.svelte
@@ -1,13 +1,20 @@
-<div id="page">
-
-</div>
-
 <script>
-    import Nav from './Nav.svelte';
-    import Main from './Main.svelte';
+    import Nav from "./Nav.svelte";
+    import Main from "./Main.svelte";
 </script>
 
+<div id="page" class="grid">
+    
+</div>
+
 <style>
+    .grid {
+        background-image: linear-gradient(to top, var(--rgba-grey) 1px, transparent 1px),
+            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px);
+        background-size: 
+            10vw 10vh
+            ;
+    }
     #page {
         width: 100%;
         height: 100%;
