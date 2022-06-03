08b6089789624885eefdfebc774c618207d4d439
kyr-min <mkr200477@gmail.com>
2022-06-04 01:07:14 +0900

    page1 content1 (not for mobile)

--- src/Main.svelte
+++ src/Main.svelte
@@ -1,10 +1,53 @@
 <main>
-    <!-- <div id="text">
-        
+    
+    <div id="page1_cont1">
+        <div id="content1Box">
+            <div id="textBox">
+                <p class="sc5 cream" style="font-size: 24px">선린인터넷 고등학교 최대규모 대회</p>
+                <p class="sc7 cream" style="font-size: 50px">Sunrinthon</p>
+            </div>
+            <div id="assign">
+                <div id="linkBox">
+                    <span class="sc7" style="font-size: 28px">신청하기</span>
+                </div>
+            </div>
+        </div>
     </div>
-    <div id="floating"></div> -->
+    <div id="page1_cont2">
+
+    </div>
 </main>
 
 <style>
+    main {
+        display: flex;
+        height: 75%;
+    }
 
+    #page1_cont1 {
+        width: 45vw;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+    }
+    #page1_cont2 {
+        width: 55vw;
+        height: 100%;
+        background-color: red;
+    }
+
+    #content1Box{
+        height:100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-around;
+    }
+
+    #linkBox{
+        width: fit-content;
+        background-color: #F7E79E;
+        color: #A07F4E;
+        border-radius: 5px;
+        padding: 5px 40px 5px;
+    }
 </style>
--- src/Page1.svelte
+++ src/Page1.svelte
@@ -4,12 +4,16 @@
 </script>
 
 <div id="page" class="grid">
-    
+    <div id="tempNav">tempNav</div>
+    <Main></Main>
 </div>
 
 <style>
-    
-
+    #tempNav{
+        width: 100vw;
+        height: 15%;
+        background-color: white;
+    }
     #page {
         width: 100%;
         height: 100%;
