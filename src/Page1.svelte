248afdb7e1c159f9f5ac716fdc84432b99f90fed
kyr-min <mkr200477@gmail.com>
2022-06-03 23:39:14 +0900

    change in grey opacity

--- src/Page1.svelte
+++ src/Page1.svelte
@@ -8,41 +8,7 @@
 </div>
 
 <style>
-    .grid {
-        background-image: linear-gradient(to top, var(--rgba-grey) 1px, transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px , transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px),
-            linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px);
-        background-size: 
-            10vw 10vh,
-            10vw 10vh,
-            15vw 10vh,
-            25vw 10vh,
-            35vw 10vh,
-            55vw 10vh,
-            65vw 10vh,
-            75vw 10vh,
-            85vw 10vh;
-    }
-
-    @media only screen and (max-width: 1200px) {
-        .grid {
-            background-image: linear-gradient(to top, var(--rgba-grey) 1px, transparent 1px),
-                linear-gradient(to left, var(--rgba-grey) 1px , transparent 1px),
-                linear-gradient(to left, var(--rgba-grey) 1px, transparent 1px),
-                linear-gradient(to right, rgba(0,0,0,0) 50%,var(--rgba-grey) 1px, transparent 1px);
-            background-size: 
-                25vw 10vh,
-                25vw 10vh,
-                100vw 10vh,
-                100vw 10vh;
-        }
-    }
+    
 
     #page {
         width: 100%;
