e66effc04af32dbefa0c72b0d023b43c5d1afc8f
rumun8487 <rumun8487@naver.com>
2022-06-09 22:46:05 +0900

    change CardNews

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import SideBar from './SideBar.svelte';
 import Team_list from './Team_list.svelte';
 import Page2 from './Page2.svelte';
 import Dummy from './Dummy.svelte';
+import Card_news from './Card_News.svelte';
 
 const Sidebar = new SideBar ({
 	target: document.body
@@ -16,14 +17,15 @@ const TimerSchedule = new Page2({
 	target: document.body
 })
 
-const DummyPage = new Dummy ({
-	target:document.body
-})
 const TeamList = new Team_list({
 	target: document.body
 })
 const CardNews = new Card_news({
 	target: document.body
+})
+
+const DummyPage = new Dummy ({
+	target:document.body
 })
 
 
